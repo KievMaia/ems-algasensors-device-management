1bc007e67c6e9249eef883e84b788ce808e398ee
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-13 18:26:02 -0300

    Implementando o serviço do sensor monitoring.

--- src/main/java/br/com/kiev/device/management/domain/repository/SensorRepository.java
+++ src/main/java/br/com/kiev/device/management/domain/repository/SensorRepository.java
@@ -3,6 +3,8 @@ package br.com.kiev.device.management.domain.repository;
 import br.com.kiev.device.management.domain.model.Sensor;
 import br.com.kiev.device.management.domain.model.SensorId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SensorRepository extends JpaRepository<Sensor, SensorId> {
 }
