9615d03ecbe4c0fee13dc1bd1201d239c1293ac9
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-13 16:43:48 -0300

    Retira import sem uso.

--- src/main/java/br/com/kiev/device/management/api/controller/SensorController.java
+++ src/main/java/br/com/kiev/device/management/api/controller/SensorController.java
@@ -7,7 +7,6 @@ import br.com.kiev.device.management.domain.model.Sensor;
 import br.com.kiev.device.management.domain.model.SensorId;
 import br.com.kiev.device.management.domain.repository.SensorRepository;
 import io.hypersistence.tsid.TSID;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
