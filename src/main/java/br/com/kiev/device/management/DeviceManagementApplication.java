9581ce58341876cdf2652533dc1f62c73c49e175
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-16 12:24:24 -0300

    Implementando o serviço do sensor monitoring retornando o temperature log.

--- src/main/java/br/com/kiev/device/management/DeviceManagementApplication.java
+++ src/main/java/br/com/kiev/device/management/DeviceManagementApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class DeviceManagementApplication {
 
 	public static void main(String[] args) {
