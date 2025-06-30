60fe6b6f76811cc52f81c241778f1bfcc1b786a0
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-30 17:56:44 -0300

    Refatora a criação do RestClient utilizando o padrão Factory Pattern e implementa get details do sensor.

--- src/main/java/br/com/kiev/device/management/api/client/ISensorMonitoringClient.java
+++ src/main/java/br/com/kiev/device/management/api/client/ISensorMonitoringClient.java
@@ -2,9 +2,20 @@ package br.com.kiev.device.management.api.client;
 
 import br.com.kiev.device.management.api.model.reponse.SensorMonitoringOutput;
 import io.hypersistence.tsid.TSID;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
+@HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface ISensorMonitoringClient {
+
+    @PutExchange("/enable")
     void enableMonitoring(TSID sensorId);
+
+    @DeleteExchange("/enable")
     void disableMonitoring(TSID sensorId);
+
+    @GetExchange
     SensorMonitoringOutput getDetail(TSID sensorId);
 }
